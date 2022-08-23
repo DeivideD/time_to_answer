cc38cd0dfe50cad6410e2aa77cf650c9d8409d9d
Deivide Duarte <deivideduarte@outlook.com>
2022-08-23 20:55:21 -0300

    subject - rota create

--- app/controllers/admins_backoffice/subjects_controller.rb
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -18,7 +18,20 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
     end
   end
 
+  def create
+    @subject = Subject.new(params_admin)
+    if @subject.save
+      redirect_to admins_backoffice_subjects_path, notice: "Tema cadastrado com sucesso!"
+    else
+      render :new
+    end
+  end
+
   private 
+
+  def params_admin
+    params.require(:subject).permit(:description)
+  end
 
   def set_subject
     @subject = Subject.find(params[:id])
